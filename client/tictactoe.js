5ac9e83bcc28286bb8faec85ebcdc3faaf81309b
Aryan Agarwal <aryanagarwal@Aryans-MacBook-Air.local>
2025-07-11 13:53:12 +0530

    feat: Some frontend changes

--- client/tictactoe.js
+++ client/tictactoe.js
@@ -16,7 +16,7 @@ let playing = []; // Array to store ongoing games
 io.on("connection", (socket) => {
     // Handle the 'search' event
     socket.on("search", (e) => {
-        if (e.name != null) {       
+        if (e.name != null) {
             arr.push(e.name); // Add player name to the array
 
             // if 2 players join start game
@@ -49,17 +49,21 @@ io.on("connection", (socket) => {
 
     // Handle the 'playing' event
     socket.on("playing", (e) => {
-        if (e.value == "X") {
-            let objchange = playing.find(obj => obj.p1.p1name);
-            objchange.p1.p1move = e.id; // Update player 1 move
-            objchange.sum++;
-        } else if (e.value == "O") {
-            let objchange = playing.find(obj => obj.p2.p2name);
-            objchange.p2.p2move = e.id; // Update player 2 move
-            objchange.sum++;
+        const objchange = playing.find(obj =>
+            obj.p1.p1name === e.name ||
+            obj.p2.p2name === e.name
+        );
+        if (!objchange) return;
+
+        if (e.value === "X") {
+            objchange.p1.p1move = e.id;
+        } else {
+            objchange.p2.p2move = e.id;
         }
-        io.emit("playing", { allplayers: playing }); // Notify all clients about the game
+        objchange.sum++;
+        io.emit("playing", { allplayers: playing });
     });
+
 
     // Handle the 'makeMove' event for AI mode
     socket.on('makeMove', (board) => {
@@ -79,15 +83,15 @@ server.listen(3000, () => {
 });
 
 // function to check if there are any moves left on the board
-function moveleft(board) { 
-    for(let i = 0; i < 9; i++)
-        if (board[i] == '_') 
-            return true; 
-    return false; 
-} 
+function moveleft(board) {
+    for (let i = 0; i < 9; i++)
+        if (board[i] == '_')
+            return true;
+    return false;
+}
 
 // Evaluate the board to check for a winner
-function evaluate(board) { 
+function evaluate(board) {
     const winningCombinations = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
         [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
@@ -101,46 +105,46 @@ function evaluate(board) {
         }
     }
     return 0;//if game is going on return 0
-} 
+}
 
 // Minimax algorithm to find the best move for the AI
-function minimax(board, depth, ismax) {   
+function minimax(board, depth, ismax) {
     let score = evaluate(board); //checking if any winner
     if (score == 10) //winner is O
-        return score; 
+        return score;
     if (score == -10) //winner is X
-        return score; 
+        return score;
     if (moveleft(board) == false) //if none wins and still 0 is returned function movesleft called
         return 0; //
     if (ismax) { //if it is the turn of maximizer i.e O
-        let best = -1000; 
-        for(let i = 0; i < 9; i++) { //loop runs everytime for finding best move in each case
-            if (board[i] == '_') { 
+        let best = -1000;
+        for (let i = 0; i < 9; i++) { //loop runs everytime for finding best move in each case
+            if (board[i] == '_') {
                 board[i] = 'X'; //setting the next move for checking
                 best = Math.max(best, minimax(board, depth + 1, !ismax)); //recurrsively calling the minimax function
                 board[i] = '_'; //removing the move set for checking
-            }       
-        } 
+            }
+        }
         return best; //value of best move is returned
     } else { //move of next person i.e X returning minimum possible result
-        let best = 1000; 
-        for(let i = 0; i < 9; i++) { 
-            if (board[i] == '_') { 
+        let best = 1000;
+        for (let i = 0; i < 9; i++) {
+            if (board[i] == '_') {
                 board[i] = 'O';  //setting the next move for checking
-                best = Math.min(best, minimax(board, depth + 1, !ismax)); 
+                best = Math.min(best, minimax(board, depth + 1, !ismax));
                 board[i] = '_'; //removing the move set for checking
-            } 
-        } 
-        return best; 
-    } 
-} 
+            }
+        }
+        return best;
+    }
+}
 
 // get the best move for the AI
-function getmove(board) { 
+function getmove(board) {
     let bestvalue = -1000;
     let move = -1;  // start to -1 for no move is made
 
-    for(let i = 0; i < 9; i++) {
+    for (let i = 0; i < 9; i++) {
         if (board[i] == '_') {
             board[i] = "O";  //setting the next move for checking
             let value = minimax(board, 0, true); //calling the minimax
@@ -148,8 +152,8 @@ function getmove(board) {
             if (value > bestvalue) { //if returned value is better than the last
                 move = i; //set the index as best move
                 bestvalue = value; //the given value is set as bestvalue
-            } 
-        } 
+            }
+        }
     }
     return move;//index of best move is returned
 }
